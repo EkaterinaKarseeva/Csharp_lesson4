ee7f78178aabcbd446008a114196fef38e149e89
EkaterinaKarseeva <154433790+EkaterinaKarseeva@users.noreply.github.com>
2024-01-21 18:16:57 +0400

    3 задача

--- Program.cs
+++ Program.cs
@@ -39,63 +39,109 @@
 //}
 // Задача 2
 
+//
+
+//class Program
+//{
+    //static void Main()
+    //{
+        // Задаем размер массива
+        //int arraySize = 10; // Можете изменить размер массива по вашему усмотрению
+
+        // Создаем массив случайных трехзначных чисел
+        //int[] randomArray = GenerateRandomArray(arraySize);
+
+        // Выводим массив на консоль
+        //Console.WriteLine("Сгенерированный массив:");
+        //PrintArray(randomArray);
+
+        // Получаем количество четных чисел в массиве
+        //int evenCount = CountEvenNumbers(randomArray);
+
+        // Выводим результат
+        //Console.WriteLine($"Количество четных чисел в массиве: {evenCount}");
+    //}
+
+    //static int[] GenerateRandomArray(int size)
+    //{
+        //Random random = new Random();
+        //int[] array = new int[size];
+
+        //for (int i = 0; i < size; i++)
+        //{
+            //array[i] = random.Next(100, 1000); // Генерируем трехзначные числа
+        //}
+
+        //return array;
+    //}
+
+    //static void PrintArray(int[] array)
+    //{
+        //foreach (var number in array)
+        //{
+            //Console.Write($"{number} ");
+        //}
+        //Console.WriteLine();
+    //}
+
+    //static int CountEvenNumbers(int[] array)
+    //{
+        //int evenCount = 0;
+
+        //foreach (var number in array)
+        //{
+            //if (number % 2 == 0)
+            //{
+                //evenCount++;
+            //}
+        //}
+
+        //return evenCount;
+    //}
+//}
+
+// Задача 3
+
 using System;
 
 class Program
 {
     static void Main()
     {
-        // Задаем размер массива
-        int arraySize = 10; // Можете изменить размер массива по вашему усмотрению
+        // Задаем массив
+        int[] originalArray = { 1, 2, 3, 4, 5 };
 
-        // Создаем массив случайных трехзначных чисел
-        int[] randomArray = GenerateRandomArray(arraySize);
+        // Выводим исходный массив
+        Console.WriteLine("Исходный массив:");
+        PrintArray(originalArray);
 
-        // Выводим массив на консоль
-        Console.WriteLine("Сгенерированный массив:");
-        PrintArray(randomArray);
+        // Переворачиваем массив
+        int[] reversedArray = ReverseArray(originalArray);
 
-        // Получаем количество четных чисел в массиве
-        int evenCount = CountEvenNumbers(randomArray);
-
-        // Выводим результат
-        Console.WriteLine($"Количество четных чисел в массиве: {evenCount}");
+        // Выводим перевернутый массив
+        Console.WriteLine("Перевернутый массив:");
+        PrintArray(reversedArray);
     }
 
-    static int[] GenerateRandomArray(int size)
+    static int[] ReverseArray(int[] array)
     {
-        Random random = new Random();
-        int[] array = new int[size];
+        int length = array.Length;
+        int[] reversedArray = new int[length];
 
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < length; i++)
         {
-            array[i] = random.Next(100, 1000); // Генерируем трехзначные числа
+            reversedArray[i] = array[length - 1 - i];
         }
 
-        return array;
+        return reversedArray;
     }
 
     static void PrintArray(int[] array)
     {
-        foreach (var number in array)
+        foreach (var element in array)
         {
-            Console.Write($"{number} ");
+            Console.Write($"{element} ");
         }
         Console.WriteLine();
-    }
-
-    static int CountEvenNumbers(int[] array)
-    {
-        int evenCount = 0;
-
-        foreach (var number in array)
-        {
-            if (number % 2 == 0)
-            {
-                evenCount++;
-            }
-        }
-
-        return evenCount;
     }
 }
