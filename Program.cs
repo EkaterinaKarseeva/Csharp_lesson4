5259ec3e93ab887f60de2687b8f5c40bfd120d26
EkaterinaKarseeva <154433790+EkaterinaKarseeva@users.noreply.github.com>
2024-01-21 18:02:33 +0400

    2 задача

--- Program.cs
+++ Program.cs
@@ -1,39 +1,101 @@
 ﻿// Задача 1
 
+//using System;
+
+//class Program
+//{
+    //static void Main()
+    //{
+       // while (true)
+        //{
+            //Console.WriteLine("Введите целое число или 'q' для завершения:");
+            //string input = Console.ReadLine();
+
+            //if (input.ToLower() == "q")
+            //{
+                //Console.WriteLine("Программа завершена.");
+                //break;
+            //}
+
+            //if (IsSumOfDigitsEven(Convert.ToInt32(input)))
+            //{
+                //Console.WriteLine($"Сумма цифр числа {input} чётная. Программа завершена.");
+                //break;
+            //}
+       // }
+   // }
+
+    //static bool IsSumOfDigitsEven(int number)
+    //{
+        //int sum = 0;
+        //while (number != 0)
+        //{
+            //sum += number % 10;
+            //number /= 10;
+        //}
+
+        //return sum % 2 == 0;
+    //}
+//}
+// Задача 2
+
 using System;
 
 class Program
 {
     static void Main()
     {
-        while (true)
-        {
-            Console.WriteLine("Введите целое число или 'q' для завершения:");
-            string input = Console.ReadLine();
+        // Задаем размер массива
+        int arraySize = 10; // Можете изменить размер массива по вашему усмотрению
 
-            if (input.ToLower() == "q")
-            {
-                Console.WriteLine("Программа завершена.");
-                break;
-            }
+        // Создаем массив случайных трехзначных чисел
+        int[] randomArray = GenerateRandomArray(arraySize);
 
-            if (IsSumOfDigitsEven(Convert.ToInt32(input)))
-            {
-                Console.WriteLine($"Сумма цифр числа {input} чётная. Программа завершена.");
-                break;
-            }
-        }
+        // Выводим массив на консоль
+        Console.WriteLine("Сгенерированный массив:");
+        PrintArray(randomArray);
+
+        // Получаем количество четных чисел в массиве
+        int evenCount = CountEvenNumbers(randomArray);
+
+        // Выводим результат
+        Console.WriteLine($"Количество четных чисел в массиве: {evenCount}");
     }
 
-    static bool IsSumOfDigitsEven(int number)
+    static int[] GenerateRandomArray(int size)
     {
-        int sum = 0;
-        while (number != 0)
+        Random random = new Random();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++)
         {
-            sum += number % 10;
-            number /= 10;
+            array[i] = random.Next(100, 1000); // Генерируем трехзначные числа
         }
 
-        return sum % 2 == 0;
+        return array;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (var number in array)
+        {
+            Console.Write($"{number} ");
+        }
+        Console.WriteLine();
+    }
+
+    static int CountEvenNumbers(int[] array)
+    {
+        int evenCount = 0;
+
+        foreach (var number in array)
+        {
+            if (number % 2 == 0)
+            {
+                evenCount++;
+            }
+        }
+
+        return evenCount;
     }
 }
