567f5c88854713ca57010fa238452b43d86ee6d5
EkaterinaKarseeva <154433790+EkaterinaKarseeva@users.noreply.github.com>
2024-01-21 18:22:30 +0400

    убрала комментарии

--- Program.cs
+++ Program.cs
@@ -1,104 +1,105 @@
 ﻿// Задача 1
 
-//using System;
+using System;
 
-//class Program
-//{
-    //static void Main()
-    //{
-       // while (true)
-        //{
-            //Console.WriteLine("Введите целое число или 'q' для завершения:");
-            //string input = Console.ReadLine();
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите целое число или 'q' для завершения:");
+            string input = Console.ReadLine();
 
-            //if (input.ToLower() == "q")
-            //{
-                //Console.WriteLine("Программа завершена.");
-                //break;
-            //}
+            if (input.ToLower() == "q")
+            {
+                Console.WriteLine("Программа завершена.");
+                break;
+            }
 
-            //if (IsSumOfDigitsEven(Convert.ToInt32(input)))
-            //{
-                //Console.WriteLine($"Сумма цифр числа {input} чётная. Программа завершена.");
-                //break;
-            //}
-       // }
-   // }
+            if (IsSumOfDigitsEven(Convert.ToInt32(input)))
+            {
+                Console.WriteLine($"Сумма цифр числа {input} чётная. Программа завершена.");
+                break;
+            }
+        }
+    }
 
-    //static bool IsSumOfDigitsEven(int number)
-    //{
-        //int sum = 0;
-        //while (number != 0)
-        //{
-            //sum += number % 10;
-            //number /= 10;
-        //}
+    static bool IsSumOfDigitsEven(int number)
+    {
+        int sum = 0;
+        while (number != 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
 
-        //return sum % 2 == 0;
-    //}
-//}
+        return sum % 2 == 0;
+    }
+
+}
 // Задача 2
 
-//
 
-//class Program
-//{
-    //static void Main()
-    //{
+
+class Program
+{
+    static void Main()
+    {
         // Задаем размер массива
-        //int arraySize = 10; // Можете изменить размер массива по вашему усмотрению
+        int arraySize = 10; // Можете изменить размер массива по вашему усмотрению
 
         // Создаем массив случайных трехзначных чисел
-        //int[] randomArray = GenerateRandomArray(arraySize);
+        int[] randomArray = GenerateRandomArray(arraySize);
 
         // Выводим массив на консоль
-        //Console.WriteLine("Сгенерированный массив:");
-        //PrintArray(randomArray);
+        Console.WriteLine("Сгенерированный массив:");
+        PrintArray(randomArray);
 
         // Получаем количество четных чисел в массиве
-        //int evenCount = CountEvenNumbers(randomArray);
+        int evenCount = CountEvenNumbers(randomArray);
 
         // Выводим результат
-        //Console.WriteLine($"Количество четных чисел в массиве: {evenCount}");
-    //}
+        Console.WriteLine($"Количество четных чисел в массиве: {evenCount}");
+    }
 
-    //static int[] GenerateRandomArray(int size)
-    //{
-        //Random random = new Random();
-        //int[] array = new int[size];
+    static int[] GenerateRandomArray(int size)
+    {
+        Random random = new Random();
+        int[] array = new int[size];
 
-        //for (int i = 0; i < size; i++)
-        //{
-            //array[i] = random.Next(100, 1000); // Генерируем трехзначные числа
-        //}
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(100, 1000); // Генерируем трехзначные числа
+        }
 
-        //return array;
-    //}
+        return array;
+    }
 
-    //static void PrintArray(int[] array)
-    //{
-        //foreach (var number in array)
-        //{
-            //Console.Write($"{number} ");
-        //}
-        //Console.WriteLine();
-    //}
+    static void PrintArray(int[] array)
+    {
+        foreach (var number in array)
+        {
+            Console.Write($"{number} ");
+        }
+        Console.WriteLine();
+    }
 
-    //static int CountEvenNumbers(int[] array)
-    //{
-        //int evenCount = 0;
+    static int CountEvenNumbers(int[] array)
+    {
+        int evenCount = 0;
 
-        //foreach (var number in array)
-        //{
-            //if (number % 2 == 0)
-            //{
+        foreach (var number in array)
+        {
+            if (number % 2 == 0)
+            {
                 //evenCount++;
-            //}
-        //}
+            }
+        }
 
-        //return evenCount;
-    //}
-//}
+        return evenCount;
+    }
+}
 
 // Задача 3
 
